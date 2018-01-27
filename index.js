6bbff54691484eac8ccdbfbe55b45cc9e9292f0e
Blake Kunishige <blake3457@yahoo.com>
2018-01-27 23:09:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ var lowercase = 'hello!'
 var mixedCase = 'Hi there!'
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase());
+  if {(string === string.toLowerCase())
+}
     return "I can't hear you!" 
   else if (string === string.toUpperCase());
     return "YES INDEED!"
