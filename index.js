81a96695eaae768b644a97b6d5e7b7f5f7f97ec5
Blake Kunishige <blake3457@yahoo.com>
2018-01-27 22:38:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if sayHiToGrandma = string.toLowerCase
     return "I can't hear you!"
-  else if `string` = string.toUpperCase
+  else if sayHiToGrandma = string.toUpperCase
     return "YES INDEED!"
 }
